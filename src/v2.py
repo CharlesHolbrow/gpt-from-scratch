ffdca799cc092feee27bc809c8e484b68485bcfc
Charles Holbrow <CharlesHolbrow@gmail.com>
2024-04-24 17:15:02 -0400

    split into bigram.py and v2.py

--- src/v2.py
+++ src/v2.py
@@ -72,12 +72,14 @@ class BigramLanguageModel(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
+        self.lm_head = nn.Linear(n_embed, vocab_size)
 
     def forward(self, idx, targets=None):
 
         #idx and targets are both (B, T) tensor of integers
-        logits = self.token_embedding_table(idx) # (B,T,C)
+        token_embeddings = self.token_embedding_table(idx) # (B,T,C)
+        logits = self.lm_head(token_embeddings) # (B,T,vocab_size)
 
         if targets is None:
             loss = None
